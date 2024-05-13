ec272ac5dbe77d14e48fd2fba444d80fe7c58020
laritimoteo <larissatimoteo.678@gmail.com>
2024-05-13 07:26:37 -0300

    Ajustando o ambiente dp sisitema

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import  Path  from 'path'
+import routers from './router'
+import dotenv from 'dotenv'
+import { threadId } from 'worker_threads'
+
+dotenv.config()
+class app {
+    constructor() {
+        this.server = express()
+        mongoose.connect(process.env.DATABASE, {})
+        this.middlewares()
+        this.routes()
+
+    }
+
+middlewares () {
+    this.server.use(cors())
+    this.server.use(
+        '/files',
+        express.static(path.resolve(__dirname, '..','uploads'))
+    )
+    this.server.use(express.json())
+    }
+    routers() {
+        this.server.use(routes)
+
+    }
+  
+}
+export default new app().server
